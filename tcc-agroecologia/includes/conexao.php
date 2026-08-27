5d6127b96f5d261c351dbf27ac3ff7619e8886fa
vitorj-fatec <vitorjgoes@gmail.com>
2026-08-27 20:44:45 -0300

    Update conexao.php

--- tcc-agroecologia/includes/conexao.php
+++ tcc-agroecologia/includes/conexao.php
@@ -1,14 +1,19 @@
 <?php
 
-$host = "localhost";
-$usuario = "root";
+$host = "sql113.infinityfree.com";
+$usuario = "if0_42411794";
 $senha = "";
 $banco = "if0_42411794_tcc_agro";
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli(
+    $host,
+    $usuario,
+    $senha,
+    $banco
+);
 
 if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    die("Erro na conexão com o banco de dados.");
 }
 
 $conn->set_charset("utf8mb4");
