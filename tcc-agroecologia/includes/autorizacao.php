198961039fa6f0223b628bccbb99462704fcba56
vitorj-fatec <vitorjgoes@gmail.com>
2026-08-27 20:51:59 -0300

    Update autorizacao.php

--- tcc-agroecologia/includes/autorizacao.php
+++ tcc-agroecologia/includes/autorizacao.php
@@ -4,14 +4,14 @@ function exigirPerfil(array $perfisPermitidos)
 {
     if (!isset($_SESSION["usuario_tipo"])) {
 
-        header("Location: /tcc%20vitor/tcc-agroecologia/login.php");
+        header("Location: /login.php");
         exit;
     }
 
     if (!in_array($_SESSION["usuario_tipo"], $perfisPermitidos, true)) {
 
         header(
-            "Location: /tcc%20vitor/tcc-agroecologia/pages/dashboard.php?erro=sem_permissao"
+            "Location: /pages/dashboard.php?erro=sem_permissao"
         );
 
         exit;
